06fd711ff1916c0f0d7865ecbe2ed9a48aa218b8
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-11-23 19:35:44 -0500

    Added license header

--- fake-apt/main.c
+++ fake-apt/main.c
@@ -1,3 +1,17 @@
+/*fake-apt "main.c"
+Copyright (C) 2021 NinjaCheetah
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -33,11 +47,11 @@ int getRand(int *var,int maxNum){
 void clrScrn(){
     // Screen clear fucntion, checks OS first for compatibility:tm:
     #ifdef _WIN32
-        system("cls");
+      system("cls");
     #elif unix
-        system("clear");
+      system("clear");
     #elif __APPLE__
-        system( "clear" );
+      system( "clear" );
     #endif
 }
 int main(void){
